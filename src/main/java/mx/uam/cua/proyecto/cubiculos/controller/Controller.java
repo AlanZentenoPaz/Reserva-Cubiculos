2ff8997d7e7825c181109e1cac8d55dff608cdcc
Alan Zenteno Paz <alan.zenteno@cua.uam.mx>
2026-03-10 13:16:02 -0600

    Commit - ALAN - Arreglo en controller de prueba

--- src/main/java/mx/uam/cua/proyecto/cubiculos/controller/Controller.java
+++ src/main/java/mx/uam/cua/proyecto/cubiculos/controller/Controller.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping("/api/test2")
-    public String test() {
+    public String test2() {
         return "API funcionando correctamente - Carlos Miguel Aguilar Chaparro";
     }
     @GetMapping("/api/test1")
-    public String test() {
+    public String test1() {
         return "API funcionando correctamente - Alan Zenteno Paz";
     }
+
 
 }
