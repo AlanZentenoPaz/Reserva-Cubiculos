298a3ac65fca7fb311fda549ac34dd2a5dbe5a38
Alan Zenteno Paz <alan.zenteno@cua.uam.mx>
2026-03-10 12:02:28 -0600

    Primer commit del proyecto

---
+++ src/main/java/mx/uam/cua/proyecto/cubiculos/CubiculosApplication.java
@@ -0,0 +1,13 @@
+package mx.uam.cua.proyecto.cubiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/uam/cua/proyecto/cubiculos/CubiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uam.cua.proyecto.cubiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
